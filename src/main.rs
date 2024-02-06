d71dcea194c46aa5695e00cfbcd85b58c75172e4
Nareshkumar Rao <contact@nareshkumarrao.com>
2024-02-06 22:52:36 +0100

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+mod gui;
 mod threading;
 
-fn main() {}
+fn main() {
+    let ui = MainWindow::new().unwrap();
+    // ui.window().set_fullscreen(true);
+    ui.run().unwrap();
+}
+
+slint::slint! {
+    export component MainWindow inherits Window{
+        Text {
+            text: "hello!";
+            color: green;
+        }
+    }
+}
